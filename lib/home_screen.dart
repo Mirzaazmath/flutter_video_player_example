ed15ca9f1804b0fccdc68186457bf4d388dc2906
Mirzaazmath <azmath9603399270@gmail.com>
2025-04-15 17:13:16 +0530

    added Home Screen UI

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,8 +7,40 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Flutter Video Player"),
-
       ),
+      body: ListView.builder(
+        itemCount: 10,
+          itemBuilder: (context,index){
+          return Padding(
+            padding: EdgeInsets.all(15),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height:70,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Icon(Icons.play_arrow,color: Colors.white,),
+                ),
+                SizedBox(width: 20,),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Video Title ${index+1}",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                      Text("Video SubTitle ${index+1}",style: TextStyle(fontSize: 16,),)
+                    ],
+                  ),
+                ),
+                Icon(Icons.more_vert)
+              ],
+            ),
+          );
+
+      }),
 
     );
   }

0a04e7c47c197d27b592423dddf29165126fcd7e
Mirzaazmath <azmath9603399270@gmail.com>
2025-04-16 15:12:17 +0530

    added double tap fast forward and backword"

--- lib/video_player_screen.dart
+++ lib/video_player_screen.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:video_player/video_player.dart';
+
 class VideoPlayerScreen extends StatefulWidget {
   const VideoPlayerScreen({super.key});
 
@@ -22,18 +23,22 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     5.0,
     10.0,
   ];
-  bool isFullScreen=false;
+  bool isFullScreen = false;
 
   @override
   void initState() {
     super.initState();
-    _videoPlayerController =  VideoPlayerController.networkUrl(Uri.parse(
-        'https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'))
+    _videoPlayerController = VideoPlayerController.networkUrl(
+        Uri.parse(
+          'https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+        ),
+      )
       ..initialize().then((_) {
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {});
       });
   }
+
   @override
   void dispose() {
     _videoPlayerController.dispose();
@@ -42,21 +47,17 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-       super.dispose();
-
+    super.dispose();
   }
 
-
-  void setOreintation(){
-
-    if(isFullScreen){
+  void setOrientation() {
+    if (isFullScreen) {
       SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
       SystemChrome.setPreferredOrientations([
         DeviceOrientation.portraitUp,
         DeviceOrientation.portraitDown,
       ]);
-
-    }else{
+    } else {
       SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
       SystemChrome.setPreferredOrientations([
         DeviceOrientation.landscapeLeft,
@@ -66,99 +67,192 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     setState(() {
       isFullScreen = !isFullScreen;
     });
-
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:isFullScreen?null: AppBar(
-        title: Text("Video Player"),
-      ),
+      appBar: isFullScreen ? null : AppBar(title: Text("Video Player")),
       body: Column(
         children: [
-          _videoPlayerController.value.isInitialized?
-          Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              SizedBox(
-                height:isFullScreen? MediaQuery.sizeOf(context).height: MediaQuery.sizeOf(context).height*0.3,
-                width: double.infinity,
-                child: VideoPlayer(
-                    _videoPlayerController,
-                ),
-              ),
-              isFullScreen? Positioned(
-                left: 0,
-                top: 0,
-                child: SafeArea(
-                  child: IconButton(onPressed: (){
-                    Navigator.pop(context);
-                  }, icon: Icon(Icons.arrow_back,color: Colors.white,size: 30,)),
-                ),
-              ):SizedBox(),
-              Padding(
-                padding:isFullScreen? const EdgeInsets.symmetric(horizontal: 20,vertical: 10):EdgeInsets.zero,
-                child: Column(
-                  children: [
-                    VideoProgressIndicator(
-                        padding: const EdgeInsets.symmetric(horizontal: 15),
-                        _videoPlayerController, allowScrubbing: true),
-                    Row(
-                      children: [
-                        Expanded(
-                          child:
-                          Align(
-                            alignment: Alignment.centerLeft,
-                            child: IconButton(onPressed: (){
-                             setState(() {
-                               _videoPlayerController.value.isPlaying
-                                   ? _videoPlayerController.pause()
-                                   : _videoPlayerController.play();
-                             });
-                            }, icon: Icon( _videoPlayerController.value.isPlaying? Icons.pause:Icons.play_arrow,color: Colors.white,size: 30,)),
+          _videoPlayerController.value.isInitialized
+              ? Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  SizedBox(
+                    height:
+                        isFullScreen
+                            ? MediaQuery.sizeOf(context).height
+                            : MediaQuery.sizeOf(context).height * 0.3,
+                    width: double.infinity,
+                    child: VideoPlayer(_videoPlayerController),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child:
+                        InkWell(
+                          onDoubleTap: ()async{
+                            final currentPosition = await _videoPlayerController.position;
+                            if (currentPosition != null) {
+                              final newPosition = currentPosition - const Duration(seconds: 10);
+                              final duration = _videoPlayerController.value.duration;
+
+                              // Make sure it doesn't go beyond the total video length
+                              if (newPosition < duration) {
+                                _videoPlayerController.seekTo(newPosition);
+                              } else {
+                                _videoPlayerController.seekTo(duration);
+                              }
+                            }
+
+                          },
+                          child: SizedBox(
+                            height: isFullScreen
+                                ? MediaQuery.sizeOf(context).height
+                                : MediaQuery.sizeOf(context).height * 0.3,
+
                           ),
                         ),
-                        PopupMenuButton<double>(
-                          initialValue: _videoPlayerController.value.playbackSpeed,
-                          tooltip: 'Playback speed',
-                          onSelected: (double speed) {
-                           setState(() {
-                             _videoPlayerController.setPlaybackSpeed(speed);
-                           });
-                          },
-                          itemBuilder: (BuildContext context) {
-                            return <PopupMenuItem<double>>[
-                              for (final double speed in _examplePlaybackRates)
-                                PopupMenuItem<double>(
-                                  value: speed,
-                                  child: Text('${speed}x'),
-                                )
-                            ];
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
+                      ),
+                      Expanded(
+                        child: InkWell(
+                          onDoubleTap: ()async{
+                            final currentPosition = await _videoPlayerController.position;
+                            if (currentPosition != null) {
+                              final newPosition = currentPosition + const Duration(seconds: 10);
+                              final duration = _videoPlayerController.value.duration;
 
-                              vertical: 12,
-                              horizontal: 16,
+                              // Make sure it doesn't go beyond the total video length
+                              if (newPosition < duration) {
+                                _videoPlayerController.seekTo(newPosition);
+                              } else {
+                                _videoPlayerController.seekTo(duration);
+                              }
+                            }
+
+
+                          },
+                          child: SizedBox(
+                            height: isFullScreen
+                                ? MediaQuery.sizeOf(context).height
+                                : MediaQuery.sizeOf(context).height * 0.3,
+
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  isFullScreen
+                      ? Positioned(
+                        left: 0,
+                        top: 0,
+                        child: SafeArea(
+                          child: IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: Icon(
+                              Icons.arrow_back,
+                              color: Colors.white,
+                              size: 30,
                             ),
-                            child: Text('${_videoPlayerController.value.playbackSpeed}x',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 18),),
                           ),
                         ),
-                        IconButton(onPressed: (){
-                          setOreintation();
-                        }, icon: Icon(Icons.fullscreen,color: Colors.white,size: 30,))
-
+                      )
+                      : SizedBox(),
+                  Padding(
+                    padding:
+                        isFullScreen
+                            ? const EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 10,
+                            )
+                            : EdgeInsets.zero,
+                    child: Column(
+                      children: [
+                        VideoProgressIndicator(
+                          padding: const EdgeInsets.symmetric(horizontal: 15),
+                          _videoPlayerController,
+                          allowScrubbing: true,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _videoPlayerController.value.isPlaying
+                                          ? _videoPlayerController.pause()
+                                          : _videoPlayerController.play();
+                                    });
+                                  },
+                                  icon: Icon(
+                                    _videoPlayerController.value.isPlaying
+                                        ? Icons.pause
+                                        : Icons.play_arrow,
+                                    color: Colors.white,
+                                    size: 30,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            PopupMenuButton<double>(
+                              initialValue:
+                                  _videoPlayerController.value.playbackSpeed,
+                              tooltip: 'Playback speed',
+                              onSelected: (double speed) {
+                                setState(() {
+                                  _videoPlayerController.setPlaybackSpeed(
+                                    speed,
+                                  );
+                                });
+                              },
+                              itemBuilder: (BuildContext context) {
+                                return <PopupMenuItem<double>>[
+                                  for (final double speed
+                                      in _examplePlaybackRates)
+                                    PopupMenuItem<double>(
+                                      value: speed,
+                                      child: Text('${speed}x'),
+                                    ),
+                                ];
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 12,
+                                  horizontal: 16,
+                                ),
+                                child: Text(
+                                  '${_videoPlayerController.value.playbackSpeed}x',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                setOrientation();
+                              },
+                              icon: Icon(
+                                Icons.fullscreen,
+                                color: Colors.white,
+                                size: 30,
+                              ),
+                            ),
+                          ],
+                        ),
                       ],
                     ),
-
-                  ],
-                ),
+                  ),
+                ],
               )
-            ],
-          )
-              : Center(
-            child: CircularProgressIndicator(),
-          )
+              : Center(child: CircularProgressIndicator()),
         ],
       ),
     );
@@ -171,7 +265,7 @@ class Constants {
   static Size size = view.physicalSize / view.devicePixelRatio;
 
   // If on the web, force physical size to 500
-  static double screenWidth =  size.width;
+  static double screenWidth = size.width;
   static double screenHeight = size.height;
 
   // Set max width for web view
